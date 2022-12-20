66c1087293f2f39c7a168a1742ffdf832d315494
BulochkaUstavshaya <bezumiezzarazno@gmail.com>
2022-12-20 22:11:25 +0500

    change class to create database

--- models/users.py
+++ models/users.py
@@ -1,12 +1,17 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional, List
+from beanie import Document, Link
+
 from models.events import Event
 
 
-class User(BaseModel):
+class User(Document):
     email: EmailStr
     password: str
-    event: Optional[List[Event]]
+    event: Optional[List[Link[Event]]]
+
+    class Settings:
+        name = "users"
 
     class Config:
         schema_extra = {
